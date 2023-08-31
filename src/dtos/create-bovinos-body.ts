cabed58642a68936f86adc9d901d08bc2078f496
Maximiliano Alves <maximilianodacruz@gmail.com>
2023-08-31 19:12:45 -0300

    add post

--- src/dtos/create-bovinos-body.ts
+++ src/dtos/create-bovinos-body.ts
@@ -1,12 +1,19 @@
+import { IsNotEmpty } from 'class-validator';
 import { CreateDianteiroBody } from './create-dianteiro-body';
 import { CreateTraseiroBody } from './create-traseiro-body';
 
 export class CreateBovinosBody {
+  @IsNotEmpty()
   nome: string;
+  @IsNotEmpty()
   data_abate: number;
+  @IsNotEmpty()
   data_desossa: number;
+  @IsNotEmpty()
   raca: string;
+  @IsNotEmpty()
   peso_carcaca: number;
+  @IsNotEmpty()
   responsavel_desossa: string;
   dianteiro: CreateDianteiroBody;
   traseiro: CreateTraseiroBody;
