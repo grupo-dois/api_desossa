99cf33c98ce42727da9e0652bc5881bfd0455426
Henrique Kalife <henrique.kalife@terceiros.zenvia.com>
2023-09-30 09:36:04 -0300

    chore: create dataloader

---
+++ dataloader.sql
@@ -0,0 +1,8 @@
+INSERT INTO bovinos (nome, data_abate, data_desossa, raca, peso_carcaca, responsavel_desossa)
+VALUES ('Boi Bandido', 20230930, 20231001, 'Jersey', 500.50, 'Henrique');
+
+INSERT INTO bovinos (nome, data_abate, data_desossa, raca, peso_carcaca, responsavel_desossa)
+VALUES ('Angus', 20230825, 20230826, 'Hereford', 600.34, 'Max');
+
+INSERT INTO users (nome, email, usuario, senha)
+VALUES ('Henrique Kalife', 'henrique@email.com', 'henrique', '1234');
